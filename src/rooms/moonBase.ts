5d94691b7853fe31818c3ae95715607dfa0f46a3
Leandro Acquati <leandroacquati@gmail.com>
2022-04-07 08:55:19 -0300

    add player movement from server

--- src/rooms/moonBase.ts
+++ src/rooms/moonBase.ts
@@ -4,6 +4,7 @@ import { State } from './schema/State'
 export class MoonBase extends Room<State> {
   onCreate() {
     this.setState(new State())
+    this.onMessage('clientMovePlayer', (client, message) => this.serverMovePlayer(client, message))
     console.log('Room Created!')
   }
 
@@ -14,6 +15,26 @@ export class MoonBase extends Room<State> {
     //   // lock this room for new users
     //   this.lock()
     // }
+  }
+
+  serverMovePlayer(client: Client, data: any) {
+    if (data === 'moveUp') {
+      this.broadcast('serverMovePlayer', 'moveUp')
+    } else if (data === 'moveRight') {
+      this.broadcast('serverMovePlayer', 'moveRight')
+    } else if (data === 'moveDown') {
+      this.broadcast('serverMovePlayer', 'moveDown')
+    } else if (data === 'moveLeft') {
+      this.broadcast('serverMovePlayer', 'moveLeft')
+    } else if (data === 'idleUp') {
+      this.broadcast('serverMovePlayer', 'idleUp')
+    } else if (data === 'idleRight') {
+      this.broadcast('serverMovePlayer', 'idleRight')
+    } else if (data === 'idleDown') {
+      this.broadcast('serverMovePlayer', 'idleDown')
+    } else if (data === 'idleLeft') {
+      this.broadcast('serverMovePlayer', 'idleLeft')
+    }
   }
 
   onLeave(client: Client) {
